17896e8689f5354de9297db4c523514032abed7d
Korosium <149795750+Korosium@users.noreply.github.com>
2026-05-08 01:07:09 -0400

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,15 @@
+/**
+ * Initialize the elements on the page before doing anything with them.
+ */
+const init = () => {
+    console.log("Hello World!");
+};
+
+/**
+ * Run this function after the page has loaded.
+ */
+const main = () => {
+    init();
+};
+
+window.onload = main;
